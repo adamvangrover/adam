ba74ff722a9e9908919c666bf8d22a7a3eb7d34a
adamvangrover <adamvangrover@gmail.com>
2025-02-20 19:16:20 -0500

    Update test_data_sources.py

--- tests/test_data_sources.py
+++ tests/test_data_sources.py
@@ -1,1 +1,50 @@
-test_data_sources.py
+# tests/test_data_sources.py
+
+import unittest
+from core.data_sources.data_sources import DataSources
+#... (import other necessary modules and classes)
+
+class TestDataSources(unittest.TestCase):
+    def setUp(self):
+        """Setup method to create an instance of DataSources."""
+        config = {
+            'api_keys': {
+                'twitter': {
+                    'consumer_key': 'YOUR_CONSUMER_KEY',
+                    'consumer_secret': 'YOUR_CONSUMER_SECRET',
+                    'access_token': 'YOUR_ACCESS_TOKEN',
+                    'access_token_secret': 'YOUR_ACCESS_TOKEN_SECRET'
+                },
+                #... (add API keys for other data sources)
+            }
+        }
+        self.data_sources = DataSources(config)
+
+    def test_get_financial_news_headlines(self):
+        """Test fetching financial news headlines."""
+        headlines = self.data_sources.get_financial_news_headlines(source="bloomberg", keywords=["technology"])
+        self.assertIsNotNone(headlines)
+        #... (add more assertions to validate the fetched headlines)
+
+    def test_get_historical_news(self):
+        """Test fetching historical news data."""
+        historical_news = self.data_sources.get_historical_news(source="reuters", keywords=["inflation"], start_date="2023-01-01", end_date="2023-12-31")
+        self.assertIsNotNone(historical_news)
+        #... (add more assertions to validate the fetched historical news)
+
+    def test_get_tweets(self):
+        """Test fetching tweets from Twitter."""
+        tweets = self.data_sources.get_tweets(query="$AAPL", sentiment="positive")
+        self.assertIsNotNone(tweets)
+        #... (add more assertions to validate the fetched tweets)
+
+    def test_get_trending_topics(self):
+        """Test fetching trending topics from Twitter."""
+        trending_topics = self.data_sources.get_trending_topics(location="New York")
+        self.assertIsNotNone(trending_topics)
+        #... (add more assertions to validate the fetched trending topics)
+
+    #... (add tests for other data source methods)
+
+if __name__ == '__main__':
+    unittest.main()
